696622537454c10b53fe2462be17da7423c1c9a6
Pari Malam <apid9755@gmail.com>
2025-06-13 00:40:29 +0800

    released 0.2

--- main.py
+++ main.py
@@ -16,24 +16,21 @@ def main():
 
     args = parser.parse_args()
 
-    banners()
-    ida_path = args.path.strip('"').strip()
-
-    if not os.path.isdir(ida_path):
-        logger.error(f"Invalid IDA path provided: {ida_path}")
-        return
-
     ida = IDA()
     ida.name = args.name
     ida.email = args.email
+    ida.path = args.path.strip('"').strip()
+    if not os.path.isdir(ida.path):
+        logger.error(f"Invalid IDA path provided: {ida.path}")
+        return
 
     now = datetime.now()
     end_year = args.end_date or (now + 10)
     ida.end_date = datetime(end_year, now.month, now.day, now.hour, now.minute, now.second).strftime("%Y-%m-%d %H:%M:%S")
 
     ida.generate_license_file()
-    ida.patch_platform_binaries(ida_path)
-    ida.move_license_file(ida_path)
+    ida.patch_platform_binaries(ida.path)
+    ida.move_license_file(ida.path)
 
 if __name__ == "__main__":
     banners()
